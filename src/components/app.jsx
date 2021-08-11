b66acb95282cbec7fa11a72e392cb2c5a0f14270
Dylan Walls <dylanwalls410@gmail.com>
2021-08-10 20:46:31 -0600

    Deleted Config folder and added Axios.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import axios  from 'axios';
 import SideNavBar from './SideNavBar/sideNavBar';
 
 class App extends Component {
-    state = { }
+    state = { 
+
+    }
+    componentDidMount(){
+        this.getFlashCardsDB();
+    }
+    async getFlashCardsDB(){
+        try{
+        let response = await axios.get('http://localhost:5000/api/deck/');
+        this.setState({
+            deck: response.data
+        });
+        }
+        catch(ex) {
+            console.log('Error in API call!');
+        }
+    }
+
     render() {
         return (
             <div className="container-fluid">
--- src/index.js
+++ src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from 'react-dom'
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css'
 
-const config = require('config');
-const mongoose = require('mongoose');
-mongoose
-    .connect(
-        config.get('mongoURI'),
-    { useNewUrlParser: true, useUnifiedTopology:true})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log('Could not connect to MongoDB. ERROR: ${err}'));
-
 ReactDOM.render(<App />, document.getElementById('root'));
