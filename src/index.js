0ac5b11ceb7da63f5f2eb74dc26918494656f5f9
Dylan Walls <dylanwalls410@gmail.com>
2021-08-10 19:44:44 -0600

    Added Mongo to connect to backend.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.css'
 
-const jsxElement = <h1>Our Flashcard App</h1>;
-console.log(jsxElement);
+const config = require('config');
+const mongoose = require('mongoose');
+mongoose
+    .connect(
+        config.get('mongoURI'),
+    { useNewUrlParser: true, useUnifiedTopology:true})
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch((err) => console.log('Could not connect to MongoDB. ERROR: ${err}'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
